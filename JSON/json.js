ba9dc0a2146d12965aea4e157ecd98155a3c21e8
Blake Bishop <94153853+BalakayBishop@users.noreply.github.com>
2022-12-13 14:05:06 -0800

    Local: simple tutorial of JSON and JS object

---
+++ JSON/json.js
@@ -0,0 +1,43 @@
+// JSON
+/*
+	JavaScript Object Notation
+	- sending and receiving data in text format
+	- syntactically like JS object
+	- lightweight data-interchange format
+	- written in name value pairs
+	- names in double quotes
+	- value pairs can or cannot be in double quotes
+	- data types: number, string, object, array, boolean, null
+*/
+
+// Creating JS Object with attributes
+let myObj = {
+	name: "John",
+	age: 27,
+	married: true,
+	childrenNames: ['Jane', 'Mark']
+}
+
+// Converting JS Obj to JSON text
+let myData = JSON.stringify(myObj)
+
+console.log(myData)
+
+// JSON text
+let newObj = '{"name": "John","age": 27,"married": true,"childrenNames": ["Jane", "Mark"]}'
+
+// Converting JSON text to JS Obj
+let newData = JSON.parse(newObj)
+
+console.log(newData)
+
+
+
+
+
+
+
+
+
+
+
